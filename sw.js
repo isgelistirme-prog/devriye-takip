d75d3743c53fa0c820d68745e232bfa1e1342b88
isgelistirme-prog <isgelistirme@karakus.com>
2026-07-29 11:41:23 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,34 @@
-const CACHE_NAME = 'Karakuş-v2';
+const CACHE_NAME = 'Karakuş-v3'; // v2 → v3
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
+  './logo.png',
   'https://unpkg.com/html5-qrcode'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  // Sadece GET isteklerini cache'le (POST istekleri API'ye gidecek)
   if (event.request.method !== 'GET') return;
-  
   event.respondWith(
     caches.match(event.request).then(cachedRes => {
       return cachedRes || fetch(event.request).then(fetchRes => {
