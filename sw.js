e2eff2d01e06cb651c8bd21e8798b8bb54e4b99f
isgelistirme-prog <isgelistirme@karakus.com>
2026-08-04 16:07:08 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,45 @@
-const CACHE_NAME = 'Karakuş-v9';
+const CACHE_NAME = 'Karakuş-v5';
+const CACHE_NAME = 'Karakuş-v7';
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './logo.png',
-  'https://unpkg.com/html5-qrcode'
+'./',
+'./index.html',
+'./style.css',
+'./app.js',
+'./manifest.json',
+'./logo.png',
+'https://unpkg.com/html5-qrcode'
 ];
 
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
+event.waitUntil(
+caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
       return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-  self.clients.claim();
+})
+);
+self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cachedRes => {
-      return cachedRes || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request.url, fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    })
-  );
+if (event.request.method !== 'GET') return;
+event.respondWith(
+caches.match(event.request).then(cachedRes => {
+return cachedRes || fetch(event.request).then(fetchRes => {
+return caches.open(CACHE_NAME).then(cache => {
+cache.put(event.request.url, fetchRes.clone());
+return fetchRes;
+});
+});
+})
+);
 });
